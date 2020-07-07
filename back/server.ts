c4228000703195190645df2fb7a138dadc82c4bb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-07 14:17:52 +0200

    prettier

--- back/server.ts
+++ back/server.ts
@@ -1,17 +1,18 @@
-const express = require('express');
-const serveIndex = require('serve-index');
+const express = require("express");
+const serveIndex = require("serve-index");
 
 const app = express();
 const port = 3000;
-const www = 'www';
+const www = "www";
 
 app.use((req, res, next) => {
-    console.log('url', req.url);
-    next();
+  console.log("url", req.url);
+  next();
 });
 
 app.use(express.static(www));
 app.use(serveIndex(www));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
