614299abf5657d405bb7fbabeb9fadcd196ec329
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-07 11:49:56 +0200

    created static web server

---
+++ back/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const serveIndex = require('serve-index');
+
+const app = express();
+const port = 3000;
+const www = 'www';
+
+app.use((req, res, next) => {
+    console.log('url', req.url);
+    next();
+});
+
+app.use(express.static(www));
+app.use(serveIndex(www));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
