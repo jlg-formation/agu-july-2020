48887ea5aba4af67267ac416ae0f9912c3bef778
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-08 13:58:18 +0200

    added home component for routing

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     LegalComponent
   ],
   imports: [
