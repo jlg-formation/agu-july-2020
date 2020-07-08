ca8dbc4905984b6fdd7de21a477a04b5fec7d362
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-08 17:01:07 +0200

    added pipe ellipsis

--- front/src/app/article/article.module.ts
+++ front/src/app/article/article.module.ts
@@ -4,13 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ArticleRoutingModule } from './article-routing.module';
 import { ListComponent } from './list/list.component';
 import { CreateComponent } from './create/create.component';
-
+import { WidgetModule } from '../widget/widget.module';
 
 @NgModule({
   declarations: [ListComponent, CreateComponent],
-  imports: [
-    CommonModule,
-    ArticleRoutingModule
-  ]
+  imports: [CommonModule, ArticleRoutingModule, WidgetModule],
 })
-export class ArticleModule { }
+export class ArticleModule {}
