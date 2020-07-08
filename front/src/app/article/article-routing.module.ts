a842090ca684b636e56c4527205d38abc26ab6e4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-08 15:00:03 +0200

    added routes articles

--- front/src/app/article/article-routing.module.ts
+++ front/src/app/article/article-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { CreateComponent } from './create/create.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'article/list', component: ListComponent },
+  { path: 'article/create', component: CreateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ArticleRoutingModule { }
+export class ArticleRoutingModule {}
