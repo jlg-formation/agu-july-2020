503494bdd24089cef4f0a1f36c3d4df8eec89d07
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-08 16:32:57 +0200

    using a service

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ArticleService {
+  articles = [
+    {
+      name: 'Tournevis',
+      price: 2.44,
+      qty: 234,
+    },
+    {
+      name: 'Tournevis Cruciforme',
+      price: 4,
+      qty: 1000,
+    },
+    {
+      name: 'Pince',
+      price: 23.3,
+      qty: 46,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 }
