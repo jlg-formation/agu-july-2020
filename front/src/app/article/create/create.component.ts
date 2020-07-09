7afd39f1cc0e2fc7a1949bc5c6d6eb8b1f4fdbc4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-09 10:47:51 +0200

    submit form ok

--- front/src/app/article/create/create.component.ts
+++ front/src/app/article/create/create.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ArticleService } from 'src/app/services/article.service';
+import { Article } from 'src/app/interfaces/article';
 
 @Component({
   selector: 'app-create',
@@ -8,14 +11,17 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class CreateComponent implements OnInit {
   f = new FormGroup({
-    name: new FormControl('', [
-      Validators.required,
-      Validators.maxLength(20),
-    ]),
+    name: new FormControl('', [Validators.required, Validators.maxLength(20)]),
     price: new FormControl(2.45, [Validators.required, Validators.min(0)]),
     qty: new FormControl(100, [Validators.required, Validators.min(0)]),
   });
-  constructor() {}
+  constructor(private router: Router, private articleService: ArticleService) {}
 
   ngOnInit(): void {}
+
+  submit(): void {
+    console.log('submit');
+    this.articleService.add(this.f.value as Article);
+    this.router.navigateByUrl('/article/list');
+  }
 }
--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Article } from '../interfaces/article';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
-  articles = [
-    {
-      name: 'Tournevis',
-      price: 2.44,
-      qty: 234,
-    },
-    {
-      name: 'Tournevis Cruciforme',
-      price: 4,
-      qty: 1000,
-    },
-    {
-      name: 'Pince',
-      price: 23.3,
-      qty: 46,
-    },
-  ];
+  articles: Article[] = this.getArticles();
 
   constructor() {}
+
+  getArticles(): Article[] {
+    const str = localStorage.getItem('articles');
+    if (!str) {
+      return [
+        {
+          name: 'Tournevis',
+          price: 2.44,
+          qty: 234,
+        },
+        {
+          name: 'Tournevis Cruciforme',
+          price: 4,
+          qty: 1000,
+        },
+        {
+          name: 'Pince',
+          price: 23.3,
+          qty: 46,
+        },
+      ];
+    }
+    return JSON.parse(str) as Article[];
+  }
+
+  save(): void {
+    localStorage.setItem('articles', JSON.stringify(this.articles));
+  }
+
+  add(article: Article): void {
+    this.articles.push(article);
+    this.save();
+  }
 }
