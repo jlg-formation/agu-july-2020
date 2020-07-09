dc7517a5f676499b264548175fc4c8b2857d149c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-09 12:15:33 +0200

    add /ws/articles web api

--- back/server.ts
+++ back/server.ts
@@ -6,10 +6,33 @@ const app = express();
 const port = 3000;
 const www = "www";
 
+const articles = [
+  {
+    name: "Tournevis",
+    price: 2.44,
+    qty: 234,
+  },
+  {
+    name: "Tournevis Cruciforme",
+    price: 4,
+    qty: 1000,
+  },
+  {
+    name: "Pince",
+    price: 23.3,
+    qty: 46,
+  },
+];
+
 app.use(cors());
+
 app.use((req, res, next) => {
   console.log("url", req.url);
   next();
+});
+
+app.get("/ws/articles", (req, res) => {
+  res.json(articles);
 });
 
 app.use(express.static(www));
