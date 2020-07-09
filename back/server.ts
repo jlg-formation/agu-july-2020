989ae9ba96606f3a4abae1af81864e12ec4cde04
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-09 14:46:15 +0200

    added back-end post

--- back/server.ts
+++ back/server.ts
@@ -1,30 +1,37 @@
 import express from "express";
 import serveIndex from "serve-index";
 import cors from "cors";
+import { Article } from "../front/src/app/interfaces/article";
 
 const app = express();
 const port = 3000;
 const www = "www";
 
-const articles = [
+const articles: Article[] = [
   {
+    id: 'a0',
     name: "Tournevis",
     price: 2.44,
     qty: 234,
   },
   {
+    id: 'a1',
     name: "Tournevis Cruciforme",
     price: 4,
     qty: 1000,
   },
   {
+    id: 'a2',
     name: "Pince",
     price: 23.3,
     qty: 46,
   },
 ];
 
+let lastId = 2;
+
 app.use(cors());
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log("url", req.url);
@@ -33,6 +40,14 @@ app.use((req, res, next) => {
 
 app.get("/ws/articles", (req, res) => {
   res.json(articles);
+});
+
+app.post("/ws/articles", (req, res) => {
+  const article: Article = req.body;
+  lastId++;
+  article.id = 'a' + lastId;
+  articles.push(article);
+  res.json(article);
 });
 
 app.use(express.static(www));
--- front/src/app/services/http-article.service.ts
+++ front/src/app/services/http-article.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ArticleService } from './article.service';
+import { Article } from '../interfaces/article';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +14,30 @@ export class HttpArticleService extends ArticleService {
   }
 
   retrieveAll(): void {
-    this.http.get('http://localhost:3000/ws/articles').subscribe();
+    this.http.get<Article[]>('http://localhost:3000/ws/articles').subscribe({
+      next: (data) => {
+        console.log('data: ', data);
+        this.articles = data;
+        this.save();
+      },
+      error: (err) => console.error(err),
+      complete: () => console.log('complete'),
+    });
+  }
+
+  refresh(): void {
+    this.retrieveAll();
+  }
+
+  add(article: Article): void {
+    super.add(article);
+    this.http.post<void>('http://localhost:3000/ws/articles', article).subscribe({
+      next: () => {
+        console.log('created done.');
+        this.refresh();
+      },
+      error: (err) => console.error(err),
+      complete: () => console.log('complete'),
+    });
   }
 }
