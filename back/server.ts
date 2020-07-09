33361b4d9fbed86cad8ca5fcce3e14a3f9e6b35b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-09 12:15:11 +0200

    add cors on server

--- back/server.ts
+++ back/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import serveIndex from "serve-index";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 const www = "www";
 
+app.use(cors());
 app.use((req, res, next) => {
   console.log("url", req.url);
   next();
