8d54430708818335c7589a47c0e2a9844db37645
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-10 11:41:06 +0200

    ng test spy

--- front/src/app/article/create/create.component.spec.ts
+++ front/src/app/article/create/create.component.spec.ts
@@ -2,23 +2,28 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateComponent } from './create.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { ArticleService } from 'src/app/services/article.service';
 
 describe('CreateComponent', () => {
   let component: CreateComponent;
   let fixture: ComponentFixture<CreateComponent>;
+  let router: Router;
+  let articleService: ArticleService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [ CreateComponent ]
-    })
-    .compileComponents();
+      declarations: [CreateComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CreateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
+    articleService = TestBed.inject(ArticleService);
   });
 
   it('should create', () => {
@@ -26,7 +31,13 @@ describe('CreateComponent', () => {
   });
 
   it('should call submit', () => {
+    const navigateSpy = spyOn(router, 'navigateByUrl');
+    const addSpy = spyOn(articleService, 'add');
+
     component.submit();
+    expect(navigateSpy).toHaveBeenCalledWith('/article/list');
+    expect(addSpy).toHaveBeenCalledWith(component.f.value);
+
     expect(component).toBeTruthy();
   });
 });
