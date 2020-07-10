d8054793f6dde030d97328b148882a38b97ca9bf
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-10 10:13:41 +0200

    ng test compile and pass

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,17 +1,23 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, fakeAsync, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
+import { Routes, Router } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { Location } from '@angular/common';
+
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 describe('AppComponent', () => {
+  let router: Router;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule.withRoutes(routes), LayoutModule],
+      declarations: [AppComponent, HomeComponent],
     }).compileComponents();
+
+    router = TestBed.inject(Router);
   }));
 
   it('should create the app', () => {
@@ -24,6 +30,20 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('front app is running!');
+    expect(compiled.querySelector('app-header span').textContent).toContain(
+      'Gestion de stock'
+    );
   });
+
+  it('should render the home page', fakeAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    router.initialNavigation();
+    router.navigateByUrl('/');
+    tick();
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('app-home h1').textContent).toContain(
+      'Gérer votre stock efficacement.'
+    );
+  }));
 });
