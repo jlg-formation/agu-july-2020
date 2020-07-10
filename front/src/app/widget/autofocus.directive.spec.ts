a6ae69e8434852b2028d90ffd95cb013e7f13875
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-10 11:24:56 +0200

    ng test coverage ok

--- front/src/app/widget/autofocus.directive.spec.ts
+++ front/src/app/widget/autofocus.directive.spec.ts
@@ -1,8 +1,34 @@
 import { AutofocusDirective } from './autofocus.directive';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: '<input appAutofocus>',
+})
+export class TestComponent {
+}
 
 describe('AutofocusDirective', () => {
-  // it('should create an instance', () => {
-  //   const directive = new AutofocusDirective();
-  //   expect(directive).toBeTruthy();
-  // });
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestComponent, AutofocusDirective],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    const compiled = fixture.nativeElement;
+    expect(component).toBeTruthy();
+    expect(document.activeElement).toBe(compiled.querySelector('input'));
+  });
 });
--- front/src/app/widget/ellipsis.pipe.spec.ts
+++ front/src/app/widget/ellipsis.pipe.spec.ts
@@ -3,6 +3,15 @@ import { EllipsisPipe } from './ellipsis.pipe';
 describe('EllipsisPipe', () => {
   it('create an instance', () => {
     const pipe = new EllipsisPipe();
+    const result = pipe.transform('qsdfqsdfqsdfqsdfqsdf')
     expect(pipe).toBeTruthy();
+    expect(result).toBe('qsdfqsdfqs...');
+  });
+
+  it('test with null', () => {
+    const pipe = new EllipsisPipe();
+    const result = pipe.transform(undefined as unknown as string)
+    expect(pipe).toBeTruthy();
+    expect(result).toBeUndefined();
   });
 });
